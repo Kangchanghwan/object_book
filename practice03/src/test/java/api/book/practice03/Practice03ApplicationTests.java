de9fb06732bc386173dc27a62ea2c9a1454ec6f7
cokebear <lgodl1598@itechcompany.kr>
2024-04-09 10:06:59 +0900

    init

---
+++ practice03/src/test/java/api/book/practice03/Practice03ApplicationTests.java
@@ -0,0 +1,13 @@
+package api.book.practice03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice03ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
